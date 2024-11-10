Lime3DS/Lime3DS

Android: Update activity-ktx from 1.8.2 to 1.9.2

src/android/app/src/main/java/io/github/lime3ds/android/fragments/HomeSettingsFragment.kt
@@ -159,7 +159,7 @@ class HomeSettingsFragment : Fragment() {
                 R.string.select_lime3ds_user_folder,
                 R.string.select_lime3ds_user_folder_home_description,
                 R.drawable.ic_home,
-                { mainActivity.openLime3DSDirectory.launch(null) },
+                { mainActivity?.openLime3DSDirectory?.launch(null) },
                 details = homeViewModel.userDir
             ),
             HomeSetting(

src/android/app/src/main/java/io/github/lime3ds/android/fragments/SelectUserDirectoryDialogFragment.kt
@@ -26,7 +26,7 @@ class SelectUserDirectoryDialogFragment : DialogFragment() {
             .setTitle(R.string.select_lime3ds_user_folder)
             .setMessage(R.string.cannot_skip_directory_description)
             .setPositiveButton(android.R.string.ok) { _: DialogInterface, _: Int ->
-                mainActivity.openLime3DSDirectory.launch(null)
+                mainActivity?.openLime3DSDirectory?.launch(null)
             }
             .show()
     }

src/android/app/src/main/java/io/github/lime3ds/android/ui/main/MainActivity.kt
@@ -300,7 +300,7 @@ class MainActivity : AppCompatActivity(), ThemeProvider {
             windowInsets
         }
 
-    val openLime3DSDirectory = registerForActivityResult<Uri, Uri>(
+    val openLime3DSDirectory = registerForActivityResult(
         ActivityResultContracts.OpenDocumentTree()
     ) { result: Uri? ->
         if (result == null) {

wrybyte/azahar

src/android/app/src/main/java/org/citra/citra_emu/fragments/HomeSettingsFragment.kt
@@ -159,7 +159,7 @@ class HomeSettingsFragment : Fragment() {
                 R.string.select_citra_user_folder,
                 R.string.select_citra_user_folder_home_description,
                 R.drawable.ic_home,
-                { mainActivity.openCitraDirectory.launch(null) },
+                { mainActivity?.openCitraDirectory?.launch(null) },
                 details = homeViewModel.userDir
             ),
             HomeSetting(

src/android/app/src/main/java/org/citra/citra_emu/fragments/SelectUserDirectoryDialogFragment.kt
@@ -26,7 +26,7 @@ class SelectUserDirectoryDialogFragment : DialogFragment() {
             .setTitle(R.string.select_citra_user_folder)
             .setMessage(R.string.cannot_skip_directory_description)
             .setPositiveButton(android.R.string.ok) { _: DialogInterface, _: Int ->
-                mainActivity.openCitraDirectory.launch(null)
+                mainActivity?.openCitraDirectory?.launch(null)
             }
             .show()
     }

src/android/app/src/main/java/org/citra/citra_emu/ui/main/MainActivity.kt
@@ -300,7 +300,7 @@ class MainActivity : AppCompatActivity(), ThemeProvider {
             windowInsets
         }
 
-    val openCitraDirectory = registerForActivityResult<Uri, Uri>(
+    val openCitraDirectory = registerForActivityResult(
         ActivityResultContracts.OpenDocumentTree()
     ) { result: Uri? ->
         if (result == null) {