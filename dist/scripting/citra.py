f21e6f968f5fb2a53f0e6f7844a0aecccf279928
Taylor Rodríguez <git.rodriguez@proton.me>
2025-03-18 06:27:12 +0000

    scripting: Add documentation
    Documentation for Citra/Azahar scripting in Python has previously been
    non-existent. Adding proper Python documentation will help make
    scripting more approachable and maintainable.

--- dist/scripting/citra.py
+++ dist/scripting/citra.py
@@ -1,24 +1,88 @@
-import struct
-import random
+# Copyright Citra Emulator Project / Azahar Emulator Project
+# Licensed under GPLv2 or any later version
+# Refer to the license.txt file included.
+
+"""
+Provide Python scripting support for the Azahar Emulator.
+
+Classes:
+    Citra: Handle reading and writing memory in Azahar.
+"""
+
+__all__ = ["Citra"]
+
 import enum
+import random
 import socket
+import struct
 
 CURRENT_REQUEST_VERSION = 1
 MAX_REQUEST_DATA_SIZE = 32
 MAX_PACKET_SIZE = 48
 
+
 class RequestType(enum.IntEnum):
-    ReadMemory = 1,
+    """
+    Indicate the RPC communication request type.
+
+    Attributes:
+        ReadMemory: Request type for reading memory from Azahar.
+        WriteMemory: Request type for writing memory to Azahar.
+    """
+
+    ReadMemory = 1
     WriteMemory = 2
+
 
 CITRA_PORT = 45987
 
+
 class Citra:
+    """
+    Handle reading and writing memory in Azahar.
+
+    Communicates to the Azahar RPC server over UDP.
+
+    Attributes:
+        address: The IP address of the RPC server.
+        socket: UDP socket object used for communicating with Azahar.
+
+    Methods:
+        is_connected() -> bool:
+            Check if the Azahar RPC server is running.
+        read_memory(read_address, read_size): -> bytes | None:
+            Read game data from a memory address in Azahar.
+        write_memory(write_address, write_contents) -> bool:
+            Write data to a memory address in Azahar.
+    """
+
     def __init__(self, address="127.0.0.1", port=CITRA_PORT):
+        """
+        Initialise the RPC client.
+
+        Args:
+            address: The IP address of the RPC server. Defaults to
+                localhost address "127.0.0.1".
+            port: The port number used to communicate with the Azahar
+                RPC server. Defaults to port 45987.
+        """
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         self.address = address
 
     def is_connected(self):
+        """
+        Check if the Azahar RPC server is running.
+
+        Returns:
+            True if it is possible to connect to Azahar, False otherwise.
+            Currently always returns True.
+
+        Examples:
+            >>> c.is_connected()
+            True
+        """
+        # TODO: Check if the server can be connected to in a meaningful way.
+        #       E.g.: The behaviour of `$ nc -vzu 127.0.0.1 45987`.
         return self.socket is not None
 
     def _generate_header(self, request_type, data_size):
@@ -26,18 +90,31 @@ class Citra:
         return (struct.pack("IIII", CURRENT_REQUEST_VERSION, request_id, request_type, data_size), request_id)
 
     def _read_and_validate_header(self, raw_reply, expected_id, expected_type):
-        reply_version, reply_id, reply_type, reply_data_size = struct.unpack("IIII", raw_reply[:4*4])
-        if (CURRENT_REQUEST_VERSION == reply_version and
-            expected_id == reply_id and
-            expected_type == reply_type and
-            reply_data_size == len(raw_reply[4*4:])):
-            return raw_reply[4*4:]
+        reply_version, reply_id, reply_type, reply_data_size = struct.unpack("IIII", raw_reply[: 4 * 4])
+        if (
+            CURRENT_REQUEST_VERSION == reply_version
+            and expected_id == reply_id
+            and expected_type == reply_type
+            and reply_data_size == len(raw_reply[4 * 4 :])
+        ):
+            return raw_reply[4 * 4 :]
         return None
 
     def read_memory(self, read_address, read_size):
-        """
-        >>> c.read_memory(0x100000, 4)
-        b'\\x07\\x00\\x00\\xeb'
+        r"""
+        Read game data from a memory address in Azahar.
+
+        Args:
+            read_address: The memory address to start reading from.
+            read_size: The number of bytes to read from the `read_address`.
+
+        Returns:
+            The data read from memory, if successful. Returns `None` if the
+            Azahar RPC server fails to respond or if no data is recieved.
+
+        Examples:
+            >>> c.read_memory(0x100000, 4)
+            b'\x07\x00\x00\xeb'
         """
         result = bytes()
         while read_size > 0:
@@ -60,15 +137,28 @@ class Citra:
         return result
 
     def write_memory(self, write_address, write_contents):
-        """
-        >>> c.write_memory(0x100000, b"\\xff\\xff\\xff\\xff")
-        True
-        >>> c.read_memory(0x100000, 4)
-        b'\\xff\\xff\\xff\\xff'
-        >>> c.write_memory(0x100000, b"\\x07\\x00\\x00\\xeb")
-        True
-        >>> c.read_memory(0x100000, 4)
-        b'\\x07\\x00\\x00\\xeb'
+        r"""
+        Write data to a memory address in Azahar.
+
+        Args:
+            write_address: The memory address to write to.
+            write_contents: The data to write write to the `write_address`.
+
+        Returns:
+            True if the write operation is successful, False otherwise.
+
+        Examples:
+            >>> c.write_memory(0x100000, b"\xff\xff\xff\xff")
+            True
+
+            >>> c.read_memory(0x100000, 4)
+            b'\xff\xff\xff\xff'
+
+            >>> c.write_memory(0x100000, b"\x07\x00\x00\xeb")
+            True
+
+            >>> c.read_memory(0x100000, 4)
+            b'\x07\x00\x00\xeb'
         """
         write_size = len(write_contents)
         while write_size > 0:
@@ -90,6 +180,9 @@ class Citra:
                 return False
         return True
 
+
 if "__main__" == __name__:
     import doctest
-    doctest.testmod(extraglobs={'c': Citra()})
+
+    test_results = doctest.testmod(extraglobs={"c": Citra()})
+    print(test_results)
